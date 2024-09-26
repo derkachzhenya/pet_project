a55f766c851a3393e851ba30e6b0335a3f485b19
Yevhen <zheka4573@gmail.com>
2024-09-26 18:49:35 +0300

    creating catalog

---
+++ resources/views/main/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <title>Pet World</title>
+</head>
+
+<body>
+
+    @include('includes.header')
+
+    @yield('main')
+
+
+    @include('includes.footer')
+
+
+</body>
+
+</html>
