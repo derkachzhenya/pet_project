14f3ae2f686cfbd4c59d7aeee2cf3a9c58ecac01
Yevhen <zheka4573@gmail.com>
2024-10-11 19:59:52 +0300

    update

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -7,8 +7,8 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css" rel="stylesheet" />  
-    <link rel="icon" href="{{ asset('logo.ico') }}" type="image/x-icon">
-    <link rel="shortcut icon" href="{{ asset('logo.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('logo.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('logo.png') }}" type="image/x-icon">
     @vite('resources/css/app.css')
     <title>Pet World</title>
 </head>
